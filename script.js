95ce6140cd292b6a3ab6ea6aa0b5da6b26f07dfa
gabriel M <gabriel_jm_3@hotmail.com>
2025-01-14 22:01:54 -0300

    gabo commit

--- script.js
+++ script.js
@@ -167,3 +167,21 @@ function nextStep(step) {
     });
   });
   
+
+  window.addEventListener('DOMContentLoaded', () => {
+    const timelineItems = document.querySelectorAll('.timeline-item');
+
+    const observer = new IntersectionObserver((entries) => {
+      entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+          entry.target.classList.add('visible');
+        } else {
+          entry.target.classList.remove('visible');
+        }
+      });
+    }, { threshold: 0.5 });
+
+    timelineItems.forEach((item) => {
+      observer.observe(item);
+    });
+  });
