5d4d43a085e1b74fe04581da32fc7866657ad128
gabriel M <gabriel_jm_3@hotmail.com>
2025-01-15 16:56:28 -0300

    gabo commit

--- script.js
+++ script.js
@@ -185,3 +185,24 @@ function nextStep(step) {
       observer.observe(item);
     });
   });
+const swiper = new Swiper('.swiper', {
+  // Optional parameters
+  direction: 'vertical',
+  loop: true,
+
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+  // And if we need scrollbar
+  scrollbar: {
+    el: '.swiper-scrollbar',
+  },
+});
